cb9c351698fc0c8c957ba3f7852bc9f1427bd852
Aleksandr <maramzinalexandr@gmail.com>
2020-04-21 11:48:47 +0100

    [!] Old array-based grid implementation has been moved into abstract_grid folder [!] First Fractal<> template based implementation has been moved into abstract_old folder [+] Prepared a folder for a new Fractal<> template based implementation

--- perimeter/abstract_grid/src/args.cpp
+++ perimeter/abstract_grid/src/args.cpp
@@ -9,16 +9,14 @@ int dealwithargs(int argc, char *argv[])
   int level;
 
   if (argc > 2)
-    NumNodes = std::atoi(argv[2]);
+    NumNodes = atoi(argv[2]);
   else
     NumNodes = 1;
 
   if (argc > 1)
-    level = std::atoi(argv[1]);
+    level = atoi(argv[1]);
   else
     level = 11;
 
   return level;
-
 }
-
